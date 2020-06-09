6aeeadff700c41d660cd862f38f74606d11010dc
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2020-06-09 12:07:53 +0100

    Update bsub_jupyter_lab.sh

--- bsub_jupyter_lab.sh
+++ bsub_jupyter_lab.sh
@@ -24,7 +24,6 @@ while getopts 'g:c:m:q:d:' OPTION; do
       ;;
     d)
       notebookdir="$OPTARG"
-      echo "notebookd dir will be $notebookdir"
       ;; 
   esac
 done
@@ -32,12 +31,13 @@ shift "$(($OPTIND -1))"
 
 echo starting bsub...
 rm -f jupyter_lab.log
+echo "notebook dir will be $notebookdir"
 bsub -G ${lsf_group} \
      -R "select[mem>$mem] rusage[mem=$mem] span[hosts=1]" \
      -M ${mem} -n ${n_cpus} \
      -o jupyter_lab.log -e jupyter_lab.log \
      -q ${queue} \
-     bash start_jupyter.sh -d notebookdir > jupyter_lab.log
+     bash start_jupyter.sh -d $notebookdir > jupyter_lab.log
 echo waiting for LSF job to start... >> jupyter_lab.log
 echo finished bsub
 echo see file jupyter_lab.log for jupyterhub IP address and port
