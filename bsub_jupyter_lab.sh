7b835b544ea9a9c765f05888e1446367d9570b96
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2020-06-09 12:59:45 +0100

    Update bsub_jupyter_lab.sh

--- bsub_jupyter_lab.sh
+++ bsub_jupyter_lab.sh
@@ -3,8 +3,11 @@ set -e
 set -u
 set -o pipefail
 
+# defaults
 notebookdir=$PWD
-while getopts 'g:c:m:q:d:' OPTION; do
+condaenv=/software/hgi/installs/anaconda3/envs/nextflow20
+
+while getopts 'g:c:m:q:d:e:' OPTION; do
   case "$OPTION" in
     g)
       lsf_group="$OPTARG"
@@ -25,6 +28,9 @@ while getopts 'g:c:m:q:d:' OPTION; do
     d)
       notebookdir="$OPTARG"
       ;; 
+    e)
+      condaenv="$OPTARG"
+      ;;
   esac
 done
 shift "$(($OPTIND -1))"
@@ -32,12 +38,13 @@ shift "$(($OPTIND -1))"
 echo starting bsub...
 rm -f jupyter_lab.log
 echo "notebook dir will be $notebookdir"
+echo "jupyter lab conda env is $condaenv"
 bsub -G ${lsf_group} \
      -R "select[mem>$mem] rusage[mem=$mem] span[hosts=1]" \
      -M ${mem} -n ${n_cpus} \
      -o jupyter_lab.log -e jupyter_lab.log \
      -q ${queue} \
-     bash start_jupyter.sh -d $notebookdir > jupyter_lab.log
+     bash start_jupyter.sh -d $notebookdir -e $condaenv > jupyter_lab.log
 echo waiting for LSF job to start... >> jupyter_lab.log
 echo finished bsub
 echo see file jupyter_lab.log for jupyterhub IP address and port
